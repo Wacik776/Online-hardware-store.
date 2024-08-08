811864a513fd060caf7b0f6aa13127ec6cd60cba
Robert <103214257+Wacik776@users.noreply.github.com>
2024-08-08 03:35:06 +0300

    add Single product Card

---
+++ src/components/Products/Product.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import styles from "../../styles/product/product.module.scss";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../utils/routes";
+
+const SIZES = [4, 4.5, 5, 5.5, 6, 6.5, 7];
+
+export const Product = ({ images, title, price, description }) => {
+  let [currentImg, setCurrentImg] = useState("Loading");
+  let [currentSize, setCurrentSize] = useState();
+  useEffect(() => {
+    if (images.length == 0) return;
+    setCurrentImg(images[0])
+  },[images]);
+  return (
+    <section className={styles.product}>
+      <div className={styles.images}>
+        <div
+          className={styles.current}
+          style={{
+            backgroundImage: `url(${currentImg})`,
+          }}
+        />
+        <div className={styles["images-list"]}>
+          {images.map((image, index) => (
+            <div
+              className={styles.image}
+              key={index}
+              style={{ backgroundImage: `url(${image})` }}
+              onClick={() => {setCurrentImg(image)}}
+            />
+          ))}
+        </div>
+      </div>
+      <div className={styles.info}>
+        <h1 className={styles.title}>{title}</h1>
+        <div className={styles.price}>{price}$</div>
+        <div className={styles.color}>
+          <span>Color:</span> Green
+        </div>
+        <div className={styles.sizes}>
+          <span>Sizes:</span>
+          <div className={styles.list}>
+            {SIZES.map((size) => (
+              <div className={styles[`size-${currentSize==size? 'active' : ''}`]} onClick={() => {setCurrentSize(size)}} key={size}>
+                {size}
+              </div>
+            ))}
+          </div>
+        </div>
+        <p className={styles.description}>{description}</p>
+        <div className={styles.actions}>
+          <button disabled={!currentSize} className={styles.add}>Add to Cart</button>
+          <button className={styles.favourite}>Add to Favourites</button>
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.purchase}>19 people purchased</div>
+          <Link to={ROUTES.HOME} className={styles.return}>
+            RETURN TO HOME {`>`}
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,14 +1,30 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useGetProductQuery } from "../../features/api/apiSlice";
 import { SidePoster } from "../SidePoster/SidePoster";
+import { ROUTES } from "../../utils/routes";
+import { Product } from "./Product";
 
 export const SingleProduct = () => {
   const { id } = useParams();
-  const { data } = useGetProductQuery({ id });
-  console.log(data)
+  const navigate = useNavigate();
+  const { data, isLoading, isFetching, isSuccess } = useGetProductQuery({ id });
+  useEffect(() => {
+    if (!isFetching && !isLoading && !isSuccess) {
+      navigate(ROUTES.HOME); //redirect
+    }
+  }, [isLoading, isFetching, isSuccess]);
+  console.log("data", !data);
   return (
     <div>
-        <SidePoster poster={''} />
-    </div>);
+      <div style={{display: 'flex', gap: 20}}>
+        <SidePoster poster={""} />
+        {data !== undefined ? (
+          <Product {...data} />
+        ) : (
+          <section style={{width: "100%", display: 'flex', justifyContent: "center", alignItems: "center" }}>Loading...</section>
+        )}
+      </div>
+    </div>
+  );
 };
